4502aadcd88fa53fb3702fedb5f5bdd1dceaadf8
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-01 22:02:48 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum {
+
+    public static void main(String args[]){
+
+        int n1=20;
+        int n2=30;
+        int sum=n1+n2;
+
+        System.out.println("Sum of a no=");
+        System.out.println(sum);
+    }
+    
+}
