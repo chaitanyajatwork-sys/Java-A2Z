ac81cd0cb4dae60e204bfcc8cb7c4f4bb1c0a718
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-02 15:51:18 +0530

    input/output

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a,b,sum;
+        System.out.println("Enter two numbers");
+        a=in.nextInt();
+        b=in.nextInt();
+        sum=a+b;
+        System.out.println(sum);
+
+    }
+    
+}
---
+++ lit.java
@@ -0,0 +1,6 @@
+public class lit {
+    public static void main(String[] args) {
+        
+    }
+    
+}
