62ed2c1254145f5ee70a959c022ebe565907288a
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-03 17:26:05 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class str {
+    public static void main(String[] args) {
+        System.out.println("Enter string");
+        Scanner in= new Scanner(System.in);
+        String str= in.next();
+        String str=in.nextLine();
+        System.out.println(str);
+        System.out.println(strl);
+    }
+
+    
+}
