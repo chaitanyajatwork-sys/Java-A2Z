4f3613d6b72d01dbc9c82117339ab5e073a904ce
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-03 16:08:50 +0530

    Create q2.java

---
+++ q2.java
@@ -0,0 +1,18 @@
+public class q2 {
+    public static void main(String[] args) {
+        float a=7/4*9/2;
+        //WRONG
+        //a=1.75*9/2
+        //a=15.75/2
+        //a=7.875
+
+        //RIGHT
+        //a=1*9/2
+        //a=9/2
+        //a=4.5 but since int/int=int
+        //therefore a=4 (ANSWER)
+
+        System.out.println(a);
+    }
+    
+}
