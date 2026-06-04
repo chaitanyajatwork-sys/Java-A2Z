ee874ebdc8dc8f270f703ec8ca086a3b409d9946
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-04 20:02:55 +0530

    Create firstnrev.java

---
+++ firstnrev.java
@@ -0,0 +1,9 @@
+public class firstnrev {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n;i>0;i--){
+            System.out.println(i);
+        }
+    }
+    
+}
