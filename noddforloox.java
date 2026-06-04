264abaf8028a45d727a1d90a25ea81dd98b0ff21
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-04 16:50:46 +0530

    Create noddforloox.java

---
+++ noddforloox.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class noddforloox {
+    public static void main(String[] args) {
+        System.out.println("Enter n");
+        Scanner in=new Scanner(System.in);
+        int n;
+        n=in.nextInt();
+        
+        for(int i=0;i<=n;i++){
+            System.out.println(2*i+1);
+        }
+    }
+    
+}
