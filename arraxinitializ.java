64df7732782cba14321d9358a7fbffc4640cb3c8
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-06 00:09:08 +0530

    Update arraxinitializ.java

--- arraxinitializ.java
+++ arraxinitializ.java
@@ -4,6 +4,9 @@ public class arraxinitializ {
         System.out.println("Array elements");
         for(int i=0;i<5;i++)
             System.out.println(marks[i]);
+        System.out.println(marks.length); 
+        for(int element:marks){
+            System.out.println(element);
+        }
     }
-    
 }
