e090f1724aa04da28d9a8ab1bbed98d245731ae0
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-05 14:41:09 +0530

    Create sumoffirstevenno.java

---
+++ sumoffirstevenno.java
@@ -0,0 +1,16 @@
+public class sumoffirstevenno {
+    public static void main(String[] args) {
+        int n=10;
+        int sum=0;
+        int i=0;
+        while(i<n ){
+            if(i%2==0){
+            sum=sum+i;
+          
+            }
+            i++;
+        }
+        System.out.println(sum);
+    }
+    
+}
