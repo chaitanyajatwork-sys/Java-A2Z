ac7adc972950d0568b2b85c7cc5ffe956eecb444
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-05 22:50:55 +0530

    Create arrax.java

---
+++ arrax.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class arrax {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int []marks=new int[5];
+        System.out.println("Enter array elements");
+        for(int i=0;i<5;i++){
+            marks[i]=in.nextInt();
+        }
+        System.out.println("First element");
+        System.out.println(marks[0]);
+        System.out.println("second element");
+        System.out.println(marks[1]);
+        System.out.println("thrd element");
+        System.out.println(marks[2]);
+        System.out.println("fourth element");
+        System.out.println(marks[3]);
+        System.out.println("fifth element");
+        System.out.println(marks[4]);
+    
+        
+    }
+    
+}
