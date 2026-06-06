0982a32cc38be74bd3ef9415d7ec9c082c61ea2c
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-06 21:55:13 +0530

    Create methoxOr.java

---
+++ methoxOr.java
@@ -0,0 +1,15 @@
+public class methoxOr {
+    int Demo(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        methoxOr obj= new methoxOr();
+        int a=21;
+        int b=1;
+        int sum=obj.Demo(a,b);
+        System.out.println(sum);
+        
+    
+    }
+    
+}
