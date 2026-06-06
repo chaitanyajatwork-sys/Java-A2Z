fe48a89c44fc0c6345b96ee68d356061540b38f6
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-06 14:39:43 +0530

    Create findnoinarrax.java

---
+++ findnoinarrax.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class findnoinarrax {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter 5 elements of array");
+        int [] arr= new int[5];
+        int sum=0;
+        int find;
+        int findno;
+        int flag=0;
+        int i;
+        for(int j=0;j<5;j++){
+            arr[j]=in.nextInt();
+        }
+        System.out.println("enter the integer to find");
+        find=in.nextInt();
+        for(i=0;i<5;i++){
+            if(find==arr[i]){
+                flag=1;
+                findno=i;
+                break;
+            }
+        }
+        if(flag==1){
+            System.out.printf("element found at index %d",i+1);
+        }
+        else
+            System.out.println("not found");
+
+    }
+}   
