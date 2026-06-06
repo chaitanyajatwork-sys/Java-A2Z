8acc1e68f73c69a8d9a0d6318156c077e2c1102c
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-06 21:46:22 +0530

    Create methox.java

---
+++ methox.java
@@ -0,0 +1,13 @@
+public class methox {
+    static int demo(int a,int b){
+        return a+b;
+
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        System.out.println(demo(a,b));
+        
+    }
+    
+}
