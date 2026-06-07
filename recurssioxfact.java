854cd835002fc0f9251c1e993ff51c0dd34bc851
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-07 15:30:07 +0530

    Create recurssioxfact.java

---
+++ recurssioxfact.java
@@ -0,0 +1,14 @@
+public class recurssioxfact {
+    static int fact(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        else
+            return n*fact(n-1);
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5:"+ fact(0));
+    }
+    
+}
