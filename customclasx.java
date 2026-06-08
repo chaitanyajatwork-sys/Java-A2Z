ad47a233b15a1ed18f56d9c25275c1de4c3bf7f5
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-08 23:57:44 +0530

    Create customclasx.java

---
+++ customclasx.java
@@ -0,0 +1,14 @@
+class employee{
+        int id;
+        String name;
+}
+public class customclasx {
+    public static void main(String[] args) {
+        employee in=new employee();
+        in.id=13;
+        in.name="cj";
+        System.out.println(in.id);
+        System.out.println(in.name);
+    }
+    
+}
