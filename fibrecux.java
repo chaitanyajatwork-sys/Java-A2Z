0b818ec43d523e2dc839020abd5b52e79a686a96
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-08 12:47:33 +0530

    Create fibrecux.java

---
+++ fibrecux.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class fibrecux {
+
+    static int feb(int n){
+        if(n==1||n==2)
+            return n-1;
+        else 
+            return feb(n-1)+feb(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int n;
+        System.out.println("enter n");
+        n=in.nextInt();
+        System.out.println(feb(n));
+
+    }
+    
+}
