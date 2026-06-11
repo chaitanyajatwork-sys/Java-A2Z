b15f29aaff59c2cb933183a2171a3b16894f277c
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-11 18:58:07 +0530

    Create cyl.java

---
+++ cyl.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class cylinder{
+    private float height;
+    private float radius;
+
+    public void setheight(float h){
+        height=h;
+    }
+
+    public void setradius(float r){
+        radius=r;
+    }
+
+    public float getheight(){
+        return height;
+    }
+
+    public float getradius(){
+        return radius;
+    }
+
+    public float surfaceArea(){
+        return 2*3.14f*radius*height+2*3.14f*radius*radius;
+    }
+
+    public float volume(){
+        return 3.1f*radius*radius*height;
+    }
+
+}
+
+
+
+public class cyl {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    cylinder in=new cylinder();
+    System.out.println("Enter height and radius of cylinder");
+    float h=sc.nextFloat();
+    float r=sc.nextFloat();
+    in.setheight(h);
+    in.setradius(r);
+
+
+    
+    System.out.println(in.getheight());
+    System.out.println(in.getradius());
+    System.out.println(in.surfaceArea());
+    System.out.println(in.volume());
+
+    }
+    
+
+    
+}
