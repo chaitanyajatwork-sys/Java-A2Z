c64a02df917946e9c38aaea1f67d10f09f3a0f7a
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-11 21:18:05 +0530

    Update cyl.java

--- cyl.java
+++ cyl.java
@@ -34,19 +34,37 @@ class cylinder{
         return 3.1f*radius*radius*height;
     }
 
+
+
 }
 
+class rec{
+    private int height;
+    private int breadth;
+
+
+rec(int h,int b){
+    height=h;
+    breadth=b;
+}
+
+public int area(){
+    return height*breadth;
+}
+}
 
 
 public class cyl {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     cylinder in=new cylinder(12,9);
+    rec on=new rec(4,2);
     // System.out.println("Enter height and radius of cylinder");
-    // float h=sc.nextFloat();
+    // float h=sc.nextFloat(); 
     // float r=sc.nextFloat();
     // in.setheight(h);
     // in.setradius(r);
+    
 
 
     
@@ -54,6 +72,7 @@ public class cyl {
     System.out.println(in.getradius());
     System.out.println(in.surfaceArea());
     System.out.println(in.volume());
+    System.out.println(on.area());
 
     }
     
