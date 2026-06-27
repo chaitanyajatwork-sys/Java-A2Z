a0c0da805500f0643363bf09d95cf8a3cf7f901d
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-06-27 22:58:50 +0530

    Update rotatearraybyk.java

---
+++ dsa/rotatearraybyk.java
@@ -0,0 +1,18 @@
+package dsa;
+
+class solu{
+    public int rotate(int nums[],int k){
+        
+
+    }
+}
+
+public class rotatearraybyk {
+    public static void main(String[] args) {
+        solu s=new solu();
+
+        int a[]={1,2,3,4,5};
+        int k=2;
+    }
+    
+}
